ed445642979bc0ef0b2b54ca4ce88ead060acbff
ThePopato <72664761+ThePopato@users.noreply.github.com>
2020-12-07 23:43:05 +0300

    Added hospital insertion system. Modified db definition to insure compatibility.

--- main.py
+++ main.py
@@ -43,3 +43,70 @@ def medicinelists_post():
     db.session.commit()
 
     return redirect(url_for('main.medicinelists'))
+
+
+@main.route('/hospitals')
+@login_required
+def hospitals():
+    return render_template('hospitals.html')
+
+@main.route('/hospitals', methods=['POST'])
+@login_required
+def hospitals_post():
+    hastaneno = request.form.get('hastaneno')
+    ilplakano = request.form.get('plakano')
+    ilismi = request.form.get('ilismi')
+    ilcepostakodu = request.form.get('postakodu')
+    ilceismi = request.form.get('ilceismi')
+    isim = request.form.get('isim')
+    hastasayisi = request.form.get('hastasayisi')
+    doktorsayisi = request.form.get('doktorsayisi')
+    olusayisi = request.form.get('olusayisi')
+
+    il=Il.query.filter_by(plakano=ilplakano).first()
+    ilce=Ilce.query.filter_by(plakano=ilplakano, postakodu=ilcepostakodu).first()
+    hastane=Hastane.query.filter_by(hastaneno = hastaneno, plakano=ilplakano, postakodu=ilcepostakodu).first()
+
+    if hastane:
+        flash('Bu bilgilere sahip bir hastane zaten var.')
+        return redirect(url_for('main.hospitals'))
+
+    if not il:
+        new_il = Il(ilplakano,ilismi, hastasayisi, doktorsayisi, olusayisi)
+        new_ilce = Ilce(ilplakano, ilcepostakodu, ilceismi, hastasayisi, doktorsayisi, olusayisi)
+        hastane=Hastane(hastaneno, ilplakano, ilcepostakodu, isim, hastasayisi, doktorsayisi, olusayisi)
+        
+        db.session.add(new_il)
+        db.session.commit()
+        
+        db.session.add(new_ilce)
+        db.session.commit()
+        
+        db.session.add(hastane)
+        db.session.commit()
+        return redirect(url_for('main.hospitals'))
+    elif il and not ilce:
+        new_ilce = Ilce(ilplakano, ilcepostakodu, ilceismi, hastasayisi, doktorsayisi, olusayisi)
+        db.session.add(new_ilce)
+        db.session.commit()
+        
+        il.hastasayisi = il.hastasayisi+int(hastasayisi)
+        il.doktorsayisi = il.doktorsayisi+int(doktorsayisi)
+        il.olusayisi = il.olusayisi+int(olusayisi)
+        
+        hastane=Hastane(hastaneno, ilplakano, ilcepostakodu, isim, hastasayisi, doktorsayisi, olusayisi)
+        db.session.add(hastane)
+        db.session.commit()
+    else:
+        hastane=Hastane(hastaneno, ilplakano, ilcepostakodu, isim, hastasayisi, doktorsayisi, olusayisi)
+        db.session.add(hastane)
+        ilce.hastasayisi = il.hastasayisi+int(hastasayisi)
+        ilce.doktorsayisi = il.doktorsayisi+int(doktorsayisi)
+        ilce.olusayisi = il.olusayisi+int(olusayisi)
+
+        il.hastasayisi = il.hastasayisi+int(hastasayisi)
+        il.doktorsayisi = il.doktorsayisi+int(doktorsayisi)
+        il.olusayisi = il.olusayisi+int(olusayisi)
+        db.session.commit()
+
+    return redirect(url_for('main.hospitals'))
