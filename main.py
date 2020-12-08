a221493a75da584dd3b31b71be0fdd7dd8f69596
ibahadiraltun <ibahadiraltun@gmail.com>
2020-12-08 13:56:36 +0300

    add update implementation for olasivakalar

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ def get_possible_cases():
 @main.route('/olasi_vakalar', methods=['POST', 'GET'])
 def olasi_vakalar():
     possible_cases = get_possible_cases()
-    print('HEYY')
-    for i in possible_cases:
-        print(i.tckn)
-
     return render_template('olasi_vakalar.html', cases = possible_cases)
 
 @main.route('/delete_olasi_vaka/<tckn>', methods=['POST', 'GET'])
@@ -35,13 +31,35 @@ def delete_olasi_vaka(tckn):
     db.session.commit()
     return redirect(url_for('main.olasi_vakalar'))
 
+@main.route('/update_olasi_vaka/<tckn>', methods=['POST', 'GET'])
+def update_olasi_vaka(tckn):
+    if request.method == 'POST':
+        form = request.form
+        values = form.to_dict(flat = True)
+        existing_case = db.session.query(OlasiVakalar).filter_by(tckn = tckn).first()
+        existing_case.tckn = values['tckn']
+        existing_case.ad = values['ad']
+        existing_case.soyad = values['soyad']
+        existing_case.telno = values['telno']
+        existing_case.evadresi = values['evadresi']
+        existing_case.isadresi = values['isadresi']
+        existing_case.testtarihi = values['testtarihi']
+        existing_case.testdurumu = int(values['testdurumu'])
+        existing_case.yas = values['yas']
+        existing_case.cinsiyet = values['cinsiyet']
+        if (values['kronikhastalik'] == '0'): existing_case.kronikhastalik = 0
+        else: existing_case.kronikhastalik = 1
+        db.session.commit()
+        return redirect(url_for('main.olasi_vakalar'))
+
+    possible_case = db.session.query(OlasiVakalar).filter_by(tckn = tckn).first()
+    return render_template('update_olasi_vaka.html', case = possible_case)
+
 @main.route('/yeni_olasi_vaka', methods=['POST', 'GET'])
 def yeni_olasi_vaka():
     if request.method == 'POST':
         form = request.form
-        print(form)
         values = form.to_dict(flat = True)
-        print(values)
         new_possible_case = OlasiVakalar(tckn = values['tckn'], ad = values['ad'], soyad = values['soyad'], 
                                         telno = values['telno'], evadresi = values['evadresi'], isadresi = values['isadresi'], 
                                         testtarihi = values['testtarihi'], testdurumu = int(values['testdurumu']),
